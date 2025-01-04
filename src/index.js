ffe93cab75922615867acbe46ddcf4734138d50b
Gaurav Singh <sumitgaurav6295@gmail.com>
2025-01-04 21:46:36 +0530

    completed consumption and sending mail

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 const express = require('express')
-const { ServerConfig } = require('./config')
+const { ServerConfig, Queue } = require('../src/config')
 const app = express()
 const apiRoutes = require('./routes')
 const mailsender = require('./config/email-config')
+
+const amqplib = require("amqplib");
+const { EmailService } = require('./services')
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue", async (data) => {
+            console.log(`${Buffer.from(data.content)}`);
+            const object = JSON.parse(`${Buffer.from(data.content)}`);
+            // const object = JSON.parse(Buffer.from(data).toString());
+            await EmailService.sendEmail("airlinenoti@gmail.com", object.recepientEmail, object.subject, object.text);
+            channel.ack(data);
+        })
+    } catch (error) {
+
+    }
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,4 +30,6 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    await connectQueue();
+    console.log("queue is up")
 });
